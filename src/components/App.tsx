f8d2dc92b60341b3406e80dc48d64c064e48386c
auroraptor <interestingbutno@yandex.ru>
2023-01-15 16:19:36 +0300

    feat: ⚙️ setting up

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import styles from "./App.module.scss";
+
+const App: FC = () => (
+  <div className={styles.app}>
+    <header className={styles.header}>My map App</header>
+  </div>
+);
+
+export default App;
